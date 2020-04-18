ff1afae13b06346ad1fcc4078fbd23ea8baf32e4
mecha2k <mecha2k@naver.com>
2020-04-18 16:07:50 +0900

    1st commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+// export default () => <div>Gatsby Hello world!</div>
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>Full-stack developer</h2>
+    </div>
+  )
+}
+
+export default IndexPage
