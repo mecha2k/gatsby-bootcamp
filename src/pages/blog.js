9390a8c9559bf5775c4f63a1396c2f154eea659b
mecha2k <mecha2k@naver.com>
2020-04-20 14:42:03 +0900

    1st commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,17 +7,25 @@ import blogStyles from "./blog.module.scss"
 const Blog = () => {
   const data = useStaticQuery(graphql`
     query {
-      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+      allMarkdownRemark {
         edges {
           node {
-            title
-            slug
-            publishedDate(formatString: "MMMM Do, YYYY")
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+            html
+            excerpt
           }
         }
       }
     }
   `)
+
+  console.log(data)
 
   return (
     <div>
@@ -26,12 +34,12 @@ const Blog = () => {
         <p>Posts will show up here later on.</p>
 
         <ol className={blogStyles.posts}>
-          {data.allContentfulBlogPost.edges.map((edge) => {
+          {data.allMarkdownRemark.edges.map(function (edge) {
             return (
               <li className={blogStyles.post}>
-                <Link to={`/blog/${edge.node.slug}`}>
-                  <h2>{edge.node.title}</h2>
-                  <p>{edge.node.publishedDate}</p>
+                <Link to={`/blog/${edge.node.fields.slug}`}>
+                  <h2>{edge.node.frontmatter.title}</h2>
+                  <p>{edge.node.frontmatter.date}</p>
                 </Link>
               </li>
             )
